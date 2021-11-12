a9fd0183ca17f52cf8c72c02efd39589763441da
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2021-11-12 10:09:11 -0500

    Cleaned up code in main.h and rectangleSolver.h

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -25,10 +25,8 @@ float findRectangleArea(LINE, LINE);
 float findSlope(POINT, POINT);
 float findLength(LINE line);
 LINE* generateRectangle(POINT points[]);
-
 //This function takes in the x and y values for a point and generate a new "point"
 POINT createPoint(int, int);
-
 //This function takes in the 4 points and checks that they are not the same (meaning there is a change that the inputs could be a rectangle)
 bool arePointsSame(POINT, POINT, POINT, POINT);
 
