d12fa21fa7224ce4d30335ca8dddd5150e74a77b
Owen Garland <Ogarland9238@conestogac.on.ca>
2021-11-15 12:42:48 -0500

    added extern functions to testing

--- PolygonCheckerTesting/PolygonCheckerTesting.cpp
+++ PolygonCheckerTesting/PolygonCheckerTesting.cpp
@@ -1,5 +1,14 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+extern "C" char* analyzeTriangle(int, int, int);
+extern "C" void AngleSolver(int, int, int);
+extern "C" double CosineLawAngleA(int, int, int);
+extern "C" double CosineLawAngleB(int, int, int);
+extern "C" double CosineLawAngleC(int, int, int);
+extern "C" bool setupValidRectanglePoints(LINE, LINE, LINE, LINE);
+
+
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
