aa65a4f4386645473e68f70cbec9d7431f31d5ac
Owen Garland <Ogarland9238@conestogac.on.ca>
2021-11-15 12:20:17 -0500

    Created Unit Test files

---
+++ PolygonCheckerTesting/PolygonCheckerTesting.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PolygonCheckerTesting
+{
+	TEST_CLASS(PolygonCheckerTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
