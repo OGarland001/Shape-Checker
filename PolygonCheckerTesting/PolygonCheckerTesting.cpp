b448e7358f959fd5bfc89eb532edac481ec0216a
Brodin Collins-Robb <Bcollinsrobb9037@conestogac.on.ca>
2021-11-15 13:49:22 -0500

    Test classes set up.

--- PolygonCheckerTesting/PolygonCheckerTesting.cpp
+++ PolygonCheckerTesting/PolygonCheckerTesting.cpp
@@ -1,23 +1,37 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "..\PolygonChecker\rectangleSolver.h"
 extern "C" char* analyzeTriangle(int, int, int);
 extern "C" void AngleSolver(int, int, int);
 extern "C" double CosineLawAngleA(int, int, int);
 extern "C" double CosineLawAngleB(int, int, int);
 extern "C" double CosineLawAngleC(int, int, int);
-extern "C" bool setupValidRectanglePoints(LINE, LINE, LINE, LINE);
-
-
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace PolygonCheckerTesting
 {
-	TEST_CLASS(PolygonCheckerTesting)
+	TEST_CLASS(TypeOfTriangleFunctionality)
 	{
 	public:
 		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(TriangleInsideAngleFunctionality)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(FourPointsFunctionality)
+	{
+	public:
+
 		TEST_METHOD(TestMethod1)
 		{
 		}
