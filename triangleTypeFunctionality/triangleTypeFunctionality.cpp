dfac9f94852c1d21667befa8a8ae75569d757e48
Eric Weng <Eweng3376@conestogac.on.ca>
2021-11-16 15:23:20 -0500

    Added setupValidRectanglePoints_SquareCheck to test cases

---
+++ triangleTypeFunctionality/triangleTypeFunctionality.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace triangleTypeFunctionality
+{
+	TEST_CLASS(triangleTypeFunctionality)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
