1318d75b95fcf5780d233d5796aab873f838ea05
car-2639 <cli4620@conestogac.on.ca>
2021-11-16 09:58:11 -0500

    Added sample generateRectangle test and extern structs

--- PolygonCheckerTesting/PolygonCheckerTesting.cpp
+++ PolygonCheckerTesting/PolygonCheckerTesting.cpp
@@ -1,12 +1,15 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "..\PolygonChecker\rectangleSolver.h"
+//#include "..\PolygonChecker\rectangleSolver.h"
+
 extern "C" char* analyzeTriangle(int, int, int);
 extern "C" void AngleSolver(int, int, int);
 extern "C" double CosineLawAngleA(int, int, int);
 extern "C" double CosineLawAngleB(int, int, int);
 extern "C" double CosineLawAngleC(int, int, int);
-
+extern "C" struct POINT { int x, y; };	//extern point struct
+extern "C" struct LINE { POINT pointA, pointB; };	//extern line struct
+extern "C" LINE* generateRectangle(POINT[]);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -15,7 +18,7 @@ namespace PolygonCheckerTesting
 	TEST_CLASS(TypeOfTriangleFunctionality)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
 		}
@@ -32,8 +35,40 @@ namespace PolygonCheckerTesting
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(generateRectangle_Returnlines)	//sample generateRectangle test
 		{
+			POINT points[4];
+			POINT p;
+			p.x = 1;
+			p.y = 2;
+			points[0] = p;
+			p.x = 4;
+			p.y = 6;
+			points[1] = p;
+			p.x = 4;
+			p.y = 2;
+			points[2] = p;
+			p.x = 1;
+			p.y = 6;
+			points[3] = p;
+
+			LINE* Lines = generateRectangle(points);
+			Assert::AreEqual(1, Lines[0].pointA.x);
+			Assert::AreEqual(2, Lines[0].pointA.y);
+			Assert::AreEqual(4, Lines[0].pointB.x);
+			Assert::AreEqual(2, Lines[0].pointB.y);
+			Assert::AreEqual(4, Lines[1].pointA.x);
+			Assert::AreEqual(2, Lines[1].pointA.y);
+			Assert::AreEqual(4, Lines[1].pointB.x);
+			Assert::AreEqual(6, Lines[1].pointB.y);
+			Assert::AreEqual(4, Lines[2].pointA.x);
+			Assert::AreEqual(6, Lines[2].pointA.y);
+			Assert::AreEqual(1, Lines[2].pointB.x);
+			Assert::AreEqual(6, Lines[2].pointB.y);
+			Assert::AreEqual(1, Lines[3].pointA.x);
+			Assert::AreEqual(6, Lines[3].pointA.y);
+			Assert::AreEqual(1, Lines[3].pointB.x);
+			Assert::AreEqual(2, Lines[3].pointB.y);
 		}
 	};
 }
