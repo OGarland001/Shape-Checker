d79315dd9225ab370431e20c0cb93a3dbdd5c5e6
Eric Weng <Eweng3376@conestogac.on.ca>
2021-11-17 10:28:06 -0500

    added setupValidRectanglePoints test cases

--- PolygonCheckerTesting/PolygonCheckerTesting.cpp
+++ PolygonCheckerTesting/PolygonCheckerTesting.cpp
@@ -1,9 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-<<<<<<< HEAD
 #include "..\PolygonChecker\triangleSolver.h"
 
-=======
 #include <stdbool.h>
 #include "..\\PolygonChecker\\main.h"
 #include "..\\PolygonChecker\\triangleSolver.h"
@@ -16,10 +14,11 @@ extern "C" void AngleSolver(int, int, int);
 extern "C" double CosineLawAngleA(int, int, int);
 extern "C" double CosineLawAngleB(int, int, int);
 extern "C" double CosineLawAngleC(int, int, int);
->>>>>>> b6d4d45ddd1bfe4c4c39fbb9e7aa95243b968008
 extern "C" struct POINT { int x, y; };	//extern point struct
 extern "C" struct LINE { POINT pointA, pointB; };	//extern line struct
 extern "C" bool setupValidRectanglePoints(LINE, LINE, LINE, LINE);
+extern "C" float findRectanglePerimeter(LINE, LINE, LINE, LINE);
+extern "C" float findRectangleArea(LINE, LINE);
 extern "C" LINE* generateRectangle(POINT[]);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -107,10 +106,10 @@ namespace PolygonCheckerTesting
 	TEST_CLASS(FourPointsFunctionality)
 	{
 	public:
-		TEST_METHOD(setupValidRectanglePoints_SquareCheck) //validate rectangle for a square
+		TEST_METHOD(setupValidRectanglePoints_1) //validate rectangle for a square
 		{
 			LINE lines[4];
-			
+
 			lines[0].pointA.x = 1;
 			lines[0].pointA.y = 1;
 			lines[0].pointB.x = 1;
@@ -134,6 +133,176 @@ namespace PolygonCheckerTesting
 			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
 			Assert::AreEqual(true, valid);
 		}
+		TEST_METHOD(setupValidRectanglePoints_2) //validate rectangle for a square with negative values
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = -1;
+			lines[0].pointA.y = -1;
+			lines[0].pointB.x = -1;
+			lines[0].pointB.y = -2;
+
+			lines[1].pointA.x = -1;
+			lines[1].pointA.y = -2;
+			lines[1].pointB.x = -2;
+			lines[1].pointB.y = -2;
+
+			lines[2].pointA.x = -2;
+			lines[2].pointA.y = -2;
+			lines[2].pointB.x = -2;
+			lines[2].pointB.y = -1;
+
+			lines[3].pointA.x = -2;
+			lines[3].pointA.y = -1;
+			lines[3].pointB.x = -1;
+			lines[3].pointB.y = -1;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(true, valid);
+		}
+
+		TEST_METHOD(setupValidRectanglePoints_3) //validate rectangle for a parallelogram
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = 1;
+			lines[0].pointA.y = 1;
+			lines[0].pointB.x = 2;
+			lines[0].pointB.y = 4;
+
+			lines[1].pointA.x = 2;
+			lines[1].pointA.y = 4;
+			lines[1].pointB.x = 5;
+			lines[1].pointB.y = 1;
+
+			lines[2].pointA.x = 5;
+			lines[2].pointA.y = 1;
+			lines[2].pointB.x = 6;
+			lines[2].pointB.y = 4;
+
+			lines[3].pointA.x = 6;
+			lines[3].pointA.y = 4;
+			lines[3].pointB.x = 1;
+			lines[3].pointB.y = 1;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(false, valid);
+		}
+
+		TEST_METHOD(setupValidRectanglePoints_4) //validate rectangle for a diagonal line
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = 1;
+			lines[0].pointA.y = 1;
+			lines[0].pointB.x = 2;
+			lines[0].pointB.y = 2;
+
+			lines[1].pointA.x = 2;
+			lines[1].pointA.y = 2;
+			lines[1].pointB.x = 3;
+			lines[1].pointB.y = 3;
+
+			lines[2].pointA.x = 3;
+			lines[2].pointA.y = 3;
+			lines[2].pointB.x = 4;
+			lines[2].pointB.y = 4;
+
+			lines[3].pointA.x = 4;
+			lines[3].pointA.y = 4;
+			lines[3].pointB.x = 1;
+			lines[3].pointB.y = 1;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(false, valid);
+		}
+
+		TEST_METHOD(setupValidRectanglePoints_5) //validate rectangle for a horizontal line
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = 1;
+			lines[0].pointA.y = 1;
+			lines[0].pointB.x = 2;
+			lines[0].pointB.y = 1;
+
+			lines[1].pointA.x = 2;
+			lines[1].pointA.y = 1;
+			lines[1].pointB.x = 3;
+			lines[1].pointB.y = 1;
+
+			lines[2].pointA.x = 3;
+			lines[2].pointA.y = 1;
+			lines[2].pointB.x = 4;
+			lines[2].pointB.y = 1;
+
+			lines[3].pointA.x = 4;
+			lines[3].pointA.y = 1;
+			lines[3].pointB.x = 1;
+			lines[3].pointB.y = 1;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(false, valid);
+		}
+
+		TEST_METHOD(setupValidRectanglePoints_6) //validate rectangle for a vertical line
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = 1;
+			lines[0].pointA.y = 1;
+			lines[0].pointB.x = 1;
+			lines[0].pointB.y = 2;
+
+			lines[1].pointA.x = 1;
+			lines[1].pointA.y = 2;
+			lines[1].pointB.x = 1;
+			lines[1].pointB.y = 3;
+
+			lines[2].pointA.x = 1;
+			lines[2].pointA.y = 3;
+			lines[2].pointB.x = 1;
+			lines[2].pointB.y = 4;
+
+			lines[3].pointA.x = 1;
+			lines[3].pointA.y = 4;
+			lines[3].pointB.x = 1;
+			lines[3].pointB.y = 1;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(false, valid);
+		}
+		TEST_METHOD(setupValidRectanglePoints_7) //validate rectangle for a slanted rectangle
+		{
+			LINE lines[4];
+
+			lines[0].pointA.x = 3;
+			lines[0].pointA.y = 3;
+			lines[0].pointB.x = 4;
+			lines[0].pointB.y = 1;
+
+			lines[1].pointA.x = 4;
+			lines[1].pointA.y = 1;
+			lines[1].pointB.x = 1;
+			lines[1].pointB.y = -1;
+
+			lines[2].pointA.x = 1;
+			lines[2].pointA.y = -1;
+			lines[2].pointB.x = 0;
+			lines[2].pointB.y = 1;
+
+			lines[3].pointA.x = 0;
+			lines[3].pointA.y = 1;
+			lines[3].pointB.x = 3;
+			lines[3].pointB.y = 3;
+
+			bool valid = setupValidRectanglePoints(lines[0], lines[1], lines[2], lines[3]);
+			Assert::AreEqual(true, valid);
+		}
+
+
+
+
 		TEST_METHOD(generateRectangle_Returnlines)	//sample generateRectangle test
 		{
 			POINT points[4];
